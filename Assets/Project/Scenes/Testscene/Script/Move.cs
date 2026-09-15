960e9d6d37256b3b1064edbbf93aa6a62403e501
MemeMaster420 <tim.skywalker@icloud.com>
2026-09-15 12:40:02 +0200

    Maussteuerung generell work in progress

--- Assets/Project/Scenes/Testscene/Script/Move.cs
+++ Assets/Project/Scenes/Testscene/Script/Move.cs
@@ -3,21 +3,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float rotationSpeed = 120f;
 
     void Update()
     {
-        float moveInput = Input.GetAxis("Vertical");
-        float turnInput = Input.GetAxis("Horizontal");
-
-        // Vorwärts und rückwärts bewegen
+        float moveVerticalInput = Input.GetAxis("Vertical");
+        float moveHorizontalInput = Input.GetAxis("Horizontal");
+        
         transform.Translate(
-            Vector3.forward * moveInput * moveSpeed * Time.deltaTime
+            Vector3.forward * moveVerticalInput * moveSpeed * Time.deltaTime
         );
-
-        // Nach links und rechts drehen
-        transform.Rotate(
-            Vector3.up * turnInput * rotationSpeed * Time.deltaTime
+        
+        transform.Translate(
+            Vector3.right * moveHorizontalInput * moveSpeed * Time.deltaTime
         );
     }
 }
