a71161be1df0fdd01d038538015c2d901ea5a17b
AnnaInDerWeltLove <annalenaboege@gmail.com>
2026-09-17 15:43:13 +0200

    PuzzleMechanisem Loop
    Erster Loop des Chore-Mechanismus abgeschlossen
    Kampfmodus abgeschlossen
    Mechanismus finden ermöglicht
    Mechanismus betätigen ermöglicht
    Mechanismussequenz eingebaut

---
+++ Assets/Project/Scripts/Inventar.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Inventar : MonoBehaviour
+{
+    [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private GameObject itemImage;
+
+    private bool hasItem = false;
+
+    private void start()
+    {
+        inventoryPanel.SetActive(false);
+        itemImage.SetActive(false);
+    }
+
+    public void AddItem()
+    {
+        hasItem = true;
+        itemImage.SetActive(true);
+        Debug.Log("Item ins Inventar aufgenommen");
+    }
+
+    public void OpenInventory()
+    {
+        inventoryPanel.SetActive(true);
+    }
+
+    public void CloseInventory()
+    {
+        inventoryPanel.SetActive(false);
+    }
+}
--- Assets/Project/Scripts/MechanismInteraction.cs
+++ Assets/Project/Scripts/MechanismInteraction.cs
@@ -1,16 +1,90 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class MechanismInteraction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private VideoPlayer cutScenePlayer;
+    [SerializeField] private GameObject cutScenePanel;
+    [SerializeField] private Movement movement;
+    [SerializeField] private GameObject itemRewardPanel;
+    [SerializeField] private GameObject inventoryPanel;
+    [SerializeField] private GameObject inventoryItemImage;
+    [SerializeField] private Inventar inventar;
+    private bool playerInside;
+    private bool mechanismActivated = false;
+    private bool sequenceRunning = false;
+
     void Start()
     {
+        cutScenePanel.SetActive(false);
+        cutScenePlayer.loopPointReached += OnCutsceneFinished;
+        itemRewardPanel.SetActive(false);
+        inventoryPanel.SetActive(false);
+        inventoryItemImage.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (playerInside && !mechanismActivated && !sequenceRunning && Input.GetKeyDown(KeyCode.E))
+        {
+           StartMechanismSequence();
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player hat das Mechanismus betreten");
+            playerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player hat das Mechanismus verlassen");
+            playerInside = false;
+        }
+    }
+
+    private void StartMechanismSequence()
+    {
+        movement.enabled = false;
+        mechanismActivated = true;
+        sequenceRunning = true;
+        Debug.Log("Mechanismus ausgelöst!");
+        cutScenePanel.SetActive(true);
+        cutScenePlayer.Play(); 
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCutsceneFinished(VideoPlayer vp)
     {
-        
+        Debug.Log("Cutscene abgeschlossen!");
+        cutScenePanel.SetActive(false);
+        itemRewardPanel.SetActive(true);
+        Invoke(nameof(ShowItemInInventory), 5f);
+    }
+    private void ShowItemInInventory()
+    {
+        inventoryPanel.SetActive(true);
+        inventoryItemImage.SetActive(true);
+        Debug.Log("Item in Inventar geschoben!");
+        movement.enabled = true;
+        sequenceRunning = false;
+        Invoke(nameof(FinishMechanismSequence), 3f);
+    }
+    private void FinishMechanismSequence()
+    {
+        inventoryPanel.SetActive(false);
+        movement.enabled = true;
+        sequenceRunning = false;
+        itemRewardPanel.SetActive(false);
+        Debug.Log("Mechanismus beendet!");
     }
 }
+
+
