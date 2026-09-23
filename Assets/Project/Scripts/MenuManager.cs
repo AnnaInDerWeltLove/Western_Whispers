790737392a9453a13fdfc3860f1002912b4790b0
MemeMaster420 <tim.skywalker@icloud.com>
2026-09-23 10:57:10 +0200

    End Knopf Build lädt noch nicht

--- Assets/Project/Scripts/MenuManager.cs
+++ Assets/Project/Scripts/MenuManager.cs
@@ -12,7 +12,12 @@ public class MenuManager : MonoBehaviour
     
     public void QuitGame()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
+        //Application.Quit();
         Debug.Log("Spiel wird beendet");
     }
     
